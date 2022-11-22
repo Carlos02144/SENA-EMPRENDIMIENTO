d8e35143627c582935fcbc53d8741cddb78935f2
Gabriel Rendon <gabriel.rendongmx@gmail.com>
2022-11-22 17:30:12 +0100

    Los objetos de mision se pueden levantar

--- Assets/Script/MisionController.cs
+++ Assets/Script/MisionController.cs
@@ -7,49 +7,22 @@ using Unity.VisualScripting;
 using UnityEngine.UIElements;
 public class MisionController : MonoBehaviour
 {
-    public bool onArea,accion, aceptarMision,completado;
-    public CharacterControler player;
-    public GameObject panelInteraccion, descripcionMision, recuentoObjetos;
-    public TextMeshProUGUI textoRecuento;
+    public bool onArea, accion, aceptarMision, completado;
     public GameObject[] objetosRecoger;
     public float currentTime;
     public int numeroObjetos;
-    public GameObject objetivo;
     void Start()
     {
         numeroObjetos = objetosRecoger.Length;
-        textoRecuento.text = $"El numero de Objetos faltantes son: {numeroObjetos}";
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterControler>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Acciones de Interracion <>
-        if (Input.GetKeyDown("e") && onArea)
-        {
-            Vector3 lookTarget = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            player.gameObject.transform.LookAt(lookTarget);
-            panelInteraccion.SetActive(false);
-            descripcionMision.SetActive(true);
-            accion = true;
-
-            player.enabled = false;
-        }
-        //</>
         if (numeroObjetos <= 0) completado = true;
         if(completado)
         {
-            
-            textoRecuento.text = "Haz Completado con Exito la Mision!";
-            if (currentTime < 4)
-            {
-                currentTime += Time.deltaTime;
-                recuentoObjetos.SetActive(true);
-
-            }
-            else recuentoObjetos.SetActive(false);
+        
         }
         
     }
@@ -58,30 +31,6 @@ public class MisionController : MonoBehaviour
         if(other.tag == "Player" && !completado)
         {
             onArea = true;
-            if (!accion) panelInteraccion.SetActive(true);
         }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            onArea = false;
-            if (!accion) panelInteraccion.SetActive(false);
-        }
-    }
-    public void No()
-    { 
-        player.enabled = true;
-        accion = false;
-        descripcionMision.SetActive(false);
-        panelInteraccion.SetActive(true);
-    }
-    public void Si()
-    {
-        aceptarMision = true;
-       
-        onArea = false;
-        recuentoObjetos.SetActive(true);
-
     }
 }
--- Assets/Script/SceneSwitcher.cs
+++ Assets/Script/SceneSwitcher.cs
@@ -5,17 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public bool start;
+    public bool start,collision;
     public string nameScene;
     public static SceneSwitcher Instance { get; set; }
     void Awake()
     {
         Instance = this;
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision == true)
+        {
+            Debug.Log("E");
+            StartScene();
+        }
+    }
     public void StartScene()
     {
         SceneManager.LoadScene(nameScene);
         start = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            collision = true;
+           
+        }
     }
     public void ExitGame()
     {
